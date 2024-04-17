9ce8a129ce9dc5cae9782c2c1bf58e07c71d4bbd
MrCosta57 <58561655+MrCosta57@users.noreply.github.com>
2024-04-17 22:47:18 +0200

    Added minIO client class

--- src/file_uploader.py
+++ src/file_uploader.py
@@ -6,6 +6,7 @@ from time import ctime
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 from src.progress import Progress
+from src.minio_client import MinioClient
 
 
 def main():
@@ -22,50 +23,19 @@ def main():
     MINIO_SECRET_KEY = "root1234"
     DATA_DIR = "data/scraped/"
 
-    secure = True
-    if "localhost" in MINIO_ENDPOINT or "127.0.0.1" in MINIO_ENDPOINT:
-        secure = False
-    client = Minio(
-        endpoint=MINIO_ENDPOINT,
-        access_key=MINIO_ACCESS_KEY,
-        secret_key=MINIO_SECRET_KEY,
-        secure=secure,
+    client = MinioClient(
+        MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, secure=False
     )
-
-    # The file to upload
-    file_name = ""
-    source_file = ""
-    if os.path.exists(DATA_DIR):
-        all_files = [DATA_DIR + f for f in os.listdir(DATA_DIR)]
-        latest = max(all_files, key=os.path.getctime)
-        source_file = latest
-        file_name = os.path.basename(latest)
-    else:
-        print("Data directory not found")
-        return
-
-    # Make the bucket if it doesn't exist.
-    found = client.bucket_exists(MINIO_BUCKET_NAME)
-    if not found:
-        client.make_bucket(MINIO_BUCKET_NAME)
-        print("Created bucket", MINIO_BUCKET_NAME)
-    else:
-        print("Bucket", MINIO_BUCKET_NAME, "already exists")
-
-    # Upload the file, renaming it in the process
-    result = client.fput_object(
+    client.upload_file(
         bucket_name=MINIO_BUCKET_NAME,
-        object_name=file_name,
-        file_path=source_file,
+        source_dir=DATA_DIR,
+        latest=True,
         content_type="application/csv",
-        progress=Progress(),
-        metadata={"creation-date": ctime(os.path.getctime(source_file))},
     )
-    print(f"\nCreated {result.object_name} object")
 
 
 if __name__ == "__main__":
     try:
         main()
     except S3Error as exc:
-        print("error occurred.", exc)
+        print("Error occurred.", exc)
---
+++ src/minio_client.py
@@ -0,0 +1,91 @@
+import time
+from minio import Minio
+import os
+from time import ctime
+from progress import Progress
+
+
+class MinioClient:
+    def __init__(self, endpoint, access_key, secret_key, secure=True):
+        self.endpoint = endpoint
+        self.client = Minio(
+            endpoint=endpoint,
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=secure,
+        )
+
+    def upload_file(
+        self,
+        bucket_name,
+        source_dir,
+        file_name="",
+        latest=True,
+        content_type="application/csv",
+    ):
+        # The file to upload
+        source_path = ""
+        if os.path.exists(source_dir):
+            if latest:
+                all_files = [source_dir + f for f in os.listdir(source_dir)]
+                latest_file = max(all_files, key=os.path.getctime)
+                source_path = latest  # data_dir + latest_filename
+                file_name = os.path.basename(latest_file)
+            else:
+                source_path = source_dir + file_name
+        else:
+            print("Data directory not found")
+            return
+
+        # Make the bucket if it doesn't exist.
+        found = self.client.bucket_exists(bucket_name)
+        if not found:
+            self.client.make_bucket(bucket_name)
+            print("Created bucket", bucket_name)
+        else:
+            print("Bucket", bucket_name, "already exists")
+
+        # Upload the file, renaming it in the process
+        result = self.client.fput_object(
+            bucket_name=bucket_name,
+            object_name=file_name,
+            file_path=source_path,
+            content_type=content_type,
+            progress=Progress(),
+            metadata={"creation-date": ctime(os.path.getctime(source_path))},
+        )
+        print(f"\nCreated {result.object_name} object")
+
+    def download_file(self, dest_dir, bucket_name, file_name="", latest=True):
+        found = self.client.bucket_exists(bucket_name)
+        if not found:
+            print("Bucket", bucket_name, "not found")
+            return
+
+        objects = self.client.list_objects(bucket_name, include_user_meta=True)
+        all_files = [
+            (
+                o.object_name,
+                time.mktime(
+                    time.strptime(
+                        o.metadata["X-Amz-Meta-Creation-Date"], "%a %b %d %H:%M:%S %Y"
+                    )
+                ),
+            )
+            for o in objects
+        ]
+        if len(all_files) == 0:
+            print("No files found in the bucket")
+            return
+
+        if latest:
+            file_name = max(all_files, key=lambda x: x[1])[0]
+
+        file_path = dest_dir + file_name
+
+        file = self.client.fget_object(
+            bucket_name=bucket_name,
+            object_name=file_name,
+            file_path=file_path,
+        )
+        return file
